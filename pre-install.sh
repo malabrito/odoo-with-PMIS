ff4c6b72fc0d2185820c3ba19009d8f75ee565cc
malabrito <mlab@Matheuss-iMac.local>
2016-12-21 10:56:49 -0200

    day 2 b

--- pre-install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install openssh-server nano git bash-completion -y
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-export LANGUAGE=pt_BR.UTF-8
-export LANG=pt_BR.UTF-8
-sudo locale-gen pt_BR pt_BR.UTF-8
-sudo dpkg-reconfigure locales
-echo "Exiting SSH... Please log in again and run install.sh"
-exit
--- step2.sh
+++ step2.sh
@@ -1,12 +1,12 @@
 # main source: https://github.com/OCA/l10n-brazil/wiki/Instala%C3%A7%C3%A3o-Odoo-v8-em-Ubuntu-Server-14.04-LTS
 echo "configuring postgresql"
-sudo adduser --system --home=/opt/odoo --group odoo
 sudo apt-get install postgresql -y
 echo "type a password for postgres when requested:"
 sudo su - postgres
 createuser --createdb --username postgres --no-createrole --no-superuser --pwprompt odoo
-exit
-sudo -u postgres psql postgres
+#exit
+#sudo -u postgres psql postgres
+psql postgres
 update pg_database set datallowconn = TRUE where datname = 'template0';
 \c template0
 update pg_database set datistemplate = FALSE where datname = 'template1';
@@ -15,22 +15,12 @@ create database template1 with template = template0 encoding = 'UTF8';
 update pg_database set datistemplate = TRUE where datname = 'template1';
 \c template1
 update pg_database set datallowconn = FALSE where datname = 'template0';
-
+\q
+exit
 
 echo "installing dependencies"
-sudo apt-get install python-dev python-yaml python-feedparser python-geoip python-imaging python-pybabel python-unicodecsv wkhtmltopdf libxml2-dev libxmlsec1-dev python-argparse python-babel python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata python-gevent python-greenlet python-jinja2 python-libxslt1 python-lxml python-mako python-markupsafe python-mock python-openid python-passlib python-psutil python-psycopg2 python-pychart python-pydot python-pyparsing python-pypdf python-ldap python-yaml python-reportlab python-requests python-simplejson python-six python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-wsgiref python-xlwt python-zsi python-dev libpq-dev poppler-utils python-pdftools antiword -y
-sudo apt-get install python-pip -y
-sudo apt-get install python-setuptools -y
-sudo pip install pyserial==2.7
-sudo pip install psycogreen==1.0
-sudo pip install pyusb==1.0.0b1
-sudo pip install qrcode==5.0.1
-sudo pip install Pillow==2.5.1
-sudo pip install boto==2.38.0
-sudo pip install oerplib==0.8.4
-sudo pip install jcconv==0.2.3
-sudo pip install pytz==2014.4
-sudo pip install num2words
+sudo apt-get install python-setuptools python-pip python-dev python-yaml python-feedparser python-geoip python-imaging python-pybabel python-unicodecsv wkhtmltopdf libxml2-dev libxmlsec1-dev python-argparse python-babel python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata python-gevent python-greenlet python-jinja2 python-libxslt1 python-lxml python-mako python-markupsafe python-mock python-openid python-passlib python-psutil python-psycopg2 python-pychart python-pydot python-pyparsing python-pypdf python-ldap python-yaml python-reportlab python-requests python-simplejson python-six python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-wsgiref python-xlwt python-zsi python-dev libpq-dev poppler-utils python-pdftools antiword -y
+sudo pip install pyserial==2.7 psycogreen==1.0 pyusb==1.0.0b1 qrcode==5.0.1 Pillow==2.5.1 boto==2.38.0 oerplib==0.8.4 jcconv==0.2.3 pytz==2014.4 num2words
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
@@ -45,21 +35,14 @@ echo "configuring odoo"
 sudo cp /opt/odoo/debian/openerp-server.conf /etc/odoo-server.conf
 sudo chown odoo: /etc/odoo-server.conf
 sudo chmod 640 /etc/odoo-server.conf
-echo "
-Altere 'db_password = False' para 'db_password = senha do postgres' (passo 6).
-Modifique a linha 'addons_path = /usr/lib/python2.7/dist-packages/openerp/addons' para 'addons_path = /opt/odoo/addons'
-Adicione a seguinte linha: logfile = /var/log/odoo/odoo-server.log"
-sudo nano /etc/odoo-server.conf
+
 sudo mkdir /var/log/odoo
 sudo chown -R odoo:root /var/log/odoo
 cd /etc/init.d/
 sudo wget https://raw.githubusercontent.com/odoo/odoo/8.0/debian/init -O odoo-server
 sudo chmod 755 /etc/init.d/odoo-server #Permissao para executar arquivo
 sudo chown root: /etc/init.d/odoo-server #Usurio root como proprietrio do arquivo
-echo "Altere:
-DAEMON=/usr/bin/odoo.py mudar para diretório /opt/odoo/odoo.py
-CONFIG=/etc/odoo/openerp-server.conf mudar para /etc/odoo-server.conf"
-sudo nano /etc/init.d/odoo-server
+
 
 echo "............    downloading Project Management Information System & OCA Brazilian localization files ............"
 
@@ -104,9 +87,10 @@ git clone https://github.com/odoo-brazil/pyxmlsec --branch master
 cd pyxmlsec
 sudo python setup.py install
 
-
-echo "Alterar:
-addons_path = /opt/odoo/addons,/opt/odoo/openerp/addons,/opt/odoo/modules/pmis,/opt/odoo/modules/oca/l10n-brazil,/opt/odoo/modules/oca/account-fiscal-rule,/opt/odoo/modules/odoo-brazil/odoo-brazil-eletronic-documents,/opt/odoo/modules/oca/server-tools,/opt/odoo/modules/oca/purchase-workflow,/opt/odoo/modules/extras,/opt/odoo/modules/extras/odoo_addons,/opt/odoo/modules/extras/odoo_addons/gantt_improvement
+echo "
+Altere 'db_password = False' para 'db_password = senha do postgres'.
+Adicione a seguinte linha: logfile = /var/log/odoo/odoo-server.log
+Modifique a linha 'addons_path = /usr/lib/python2.7/dist-packages/openerp/addons' para 'addons_path = /opt/odoo/addons,/opt/odoo/openerp/addons,/opt/odoo/modules/pmis,/opt/odoo/modules/oca/l10n-brazil,/opt/odoo/modules/oca/account-fiscal-rule,/opt/odoo/modules/odoo-brazil/odoo-brazil-eletronic-documents,/opt/odoo/modules/oca/server-tools,/opt/odoo/modules/oca/purchase-workflow,/opt/odoo/modules/extras,/opt/odoo/modules/extras/odoo_addons,/opt/odoo/modules/extras/odoo_addons/gantt_improvement
 "
 sudo nano /etc/odoo-server.conf
 sudo /etc/init.d/odoo-server start
