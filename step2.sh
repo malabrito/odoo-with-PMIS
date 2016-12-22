aea63d54922d285b7a6a377d469252bb0184da9e
malabrito <mlab@Matheuss-iMac.local>
2016-12-22 13:12:52 -0200

    day 3 a

--- step2.sh
+++ step2.sh
@@ -3,10 +3,14 @@ echo "configuring postgresql - type a password for the database user when reques
 sudo adduser --system --home=/opt/odoo --group odoo
 sudo apt-get install postgresql -y
 sudo su - postgres
+echo "Copy and paste each of the blocks separatelly followed by a RETURN
+
 createuser --createdb --username postgres --no-createrole --no-superuser --pwprompt odoo
-#exit
-#sudo -u postgres psql postgres
+
+'After this step, type a Password'
+
 psql postgres
+
 update pg_database set datallowconn = TRUE where datname = 'template0';
 \c template0
 update pg_database set datistemplate = FALSE where datname = 'template1';
@@ -16,7 +20,10 @@ update pg_database set datistemplate = TRUE where datname = 'template1';
 \c template1
 update pg_database set datallowconn = FALSE where datname = 'template0';
 \q
+
 exit
+
+"
 
 echo "installing dependencies"
 sudo apt-get install python-setuptools python-pip python-dev python-yaml python-feedparser python-geoip python-imaging python-pybabel python-unicodecsv wkhtmltopdf libxml2-dev libxmlsec1-dev python-argparse python-babel python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata python-gevent python-greenlet python-jinja2 python-libxslt1 python-lxml python-mako python-markupsafe python-mock python-openid python-passlib python-psutil python-psycopg2 python-pychart python-pydot python-pyparsing python-pypdf python-ldap python-yaml python-reportlab python-requests python-simplejson python-six python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-wsgiref python-xlwt python-zsi python-dev libpq-dev poppler-utils python-pdftools antiword -y
@@ -27,9 +34,11 @@ sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
 
 echo "configuring odoo"
-sudo su - odoo -s /bin/bash
-git clone https://www.github.com/odoo/odoo --depth 1 --branch 8.0 --single-branch .
-exit
+# sudo su - odoo -s /bin/bash
+git clone https://www.github.com/odoo/odoo --depth 1 --branch 8.0 --single-branch /opt/odoo/.
+sudo chown -R odoo: /opt/odoo/
+sudo chmod -R 640 /opt/odoo/
+#exit
 
 sudo mv ~/odoo-with-PMIS/conf_step1 /etc/odoo-server.conf
 sudo chown odoo: /etc/odoo-server.conf
