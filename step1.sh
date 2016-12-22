e09b5ff6824ff29c98931518a82f3be39c32e436
malabrito <mlab@Matheuss-iMac.local>
2016-12-22 13:21:01 -0200

    3d

--- step1.sh
+++ step1.sh
@@ -1,10 +1,10 @@
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
 mv -f odoo-with-PMIS/bashrc ~/.bashrc
-sudo chmod 755 odoo-with-PMIS/step2.sh
+sudo chmod 755 ~/odoo-with-PMIS/step2.sh
 export LANGUAGE=pt_BR.UTF-8
 export LANG=pt_BR.UTF-8
 sudo locale-gen pt_BR pt_BR.UTF-8
 sudo dpkg-reconfigure locales
-echo "Exiting SSH... Please log in again and run install.sh"
+echo "Exiting SSH... Please log in again and run step2.sh"
 exit
